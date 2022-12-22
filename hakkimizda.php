bcc2d13ed17fb6958719594372a9bd732fc299a3
kamilmert043 <kamil_mert_043@hotmail.com>
2022-12-22 22:05:38 +0300

    Project Created

---
+++ hakkimizda.php
@@ -0,0 +1,65 @@
+﻿<?php include 'header.php';
+
+$hakkimizdasor=$db->prepare("SELECT * FROM hakkimizda where hakkimizda_id=:id");
+$hakkimizdasor->execute(array(
+  'id' => 0
+));
+$hakkimizdacek=$hakkimizdasor->fetch(PDO::FETCH_ASSOC);?>
+
+	
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-title-wrap">
+					<div class="page-title-inner">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="bigtitle">Hakkımızda Sayfası</div>
+						</div>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-9"><!--Main content-->
+
+			<div class="title-bg">
+					<div class="title">Tanıtım Videosu</div>
+				</div>
+					<div class="page-content">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $hakkimizdacek['hakkimizda_video'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</div>
+
+				<div class="title-bg">
+					<div class="title"><?php echo $hakkimizdacek['hakkimizda_baslik'] ?></div>
+				</div>
+				<div class="page-content">
+				<?php echo $hakkimizdacek['hakkimizda_icerik'] ?>
+				</div>
+
+
+				<div class="title-bg">
+					<div class="title">Misyon</div>
+				</div>
+					<div class="page-content">
+
+					<blockquote><?php echo $hakkimizdacek['hakkimizda_misyon'] ?></blockquote>
+				</div>
+				
+				<div class="title-bg">
+					<div class="title">Vizyon</div>
+				</div>
+					<div class="page-content">
+
+					<blockquote><?php echo $hakkimizdacek['hakkimizda_vizyon'] ?></blockquote>
+				</div>
+			</div>
+			<!-- Sidebar Here -->
+			<?php include 'sidebar.php'; ?>
+		</div>
+		<div class="spacer"></div>
+	</div>
+	
+	<?php include 'footer.php'; ?>
---
+++ nedmin/netting/islem.php
@@ -0,0 +1,259 @@
+<?php
+ob_start();
+session_start();
+include 'baglan.php';
+
+if (isset($_POST['admingiris'])) {
+$kullanici_mail=$_POST['kullanici_mail'];
+$kullanici_password=sha1($_POST['kullanici_password']);
+
+
+$kullanicisor=$db->prepare("SELECT * FROM kullanici where kullanici_mail=:mail and kullanici_password=:password and kullanici_yetki=:yetki");
+$kullanicisor->execute(array(
+'mail' => $kullanici_mail,
+'password' => $kullanici_password,
+'yetki' => 5
+));
+$say=$kullanicisor->rowCount();
+
+if ($say==1){
+$_SESSION['kullanici_mail']=$kullanici_mail;
+header("Location:../production/index.php");
+}
+else{
+	header("Location:../production/login.php?durum=no");
+	exit;
+}
+}
+
+if (isset($_POST['genelayarkaydet'])) {
+	
+	$ayarkaydet=$db->prepare("UPDATE ayar SET
+		hakkimizda_baslik=:hakkimizda_baslik,
+		hakkimizda_icerik=:hakkimizda_icerik,
+		hakkimizda_video=:hakkimizda_video,
+		hakkimizda_misyon=:hakkimizda_misyon
+		WHERE ayar_id=0");
+
+	$update=$ayarkaydet->execute(array(
+		'hakkimizda_baslik' => $_POST['hakkimizda_baslik'],
+		'hakkimizda_icerik' => $_POST['hakkimizda_icerik'],
+		'hakkimizda_video' => $_POST['hakkimizda_video'],
+		'hakkimizda_misyon' => $_POST['hakkimizda_misyon']
+	));
+
+
+	if ($update) {
+
+		header("Location:../production/genel-ayar.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/genel-ayar.php?durum=no");
+	}
+	
+}
+
+
+if (isset($_POST['iletisimayarkaydet'])) {
+	
+	$ayarkaydet=$db->prepare("UPDATE ayar SET
+		ayar_tel=:ayar_tel,
+		ayar_gsm=:ayar_gsm,
+        ayar_faks=:ayar_faks,
+        ayar_mail=:ayar_mail,
+        ayar_ilce=:ayar_ilce,
+        ayar_il=:ayar_il,
+        ayar_adres=:ayar_adres,
+        ayar_mesai=:ayar_mesai
+		WHERE ayar_id=0");
+
+	$update=$ayarkaydet->execute(array(
+		'ayar_tel' => $_POST['ayar_tel'],
+        'ayar_gsm' => $_POST['ayar_gsm'],
+        'ayar_faks' => $_POST['ayar_faks'],
+        'ayar_mail' => $_POST['ayar_mail'],
+        'ayar_ilce' => $_POST['ayar_ilce'],
+        'ayar_il' => $_POST['ayar_il'],
+        'ayar_adres' => $_POST['ayar_adres'],
+		'ayar_mesai' => $_POST['ayar_mesai']
+	));
+
+
+	if ($update) {
+
+		header("Location:../production/iletisim-ayarlar.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/iletisim-ayarlar.php?durum=no");
+	}
+	
+}
+
+if (isset($_POST['mailayarkaydet'])) {
+	
+	$ayarkaydet=$db->prepare("UPDATE ayar SET
+		
+		ayar_analystic=:ayar_analystic,
+		ayar_maps=:ayar_maps,
+		ayar_zopim=:ayar_zopim
+		WHERE ayar_id=0");
+
+	$update=$ayarkaydet->execute(array(
+
+		'ayar_analystic' => $_POST['ayar_analystic'],
+		'ayar_maps' => $_POST['ayar_maps'],
+		'ayar_zopim' => $_POST['ayar_zopim']
+	));
+
+
+	if ($update) {
+
+		header("Location:../production/mail-ayarlar.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/mail-ayarlar.php?durum=no");
+	}
+	
+}
+
+if (isset($_POST['sosyalayarkaydet'])) {
+	
+	$ayarkaydet=$db->prepare("UPDATE ayar SET
+		ayar_facebook=:ayar_facebook,
+		ayar_twitter=:ayar_twitter,
+		ayar_google=:ayar_google,
+		ayar_youtube=:ayar_youtube
+		WHERE ayar_id=0");
+
+	$update=$ayarkaydet->execute(array(
+		'ayar_facebook' => $_POST['ayar_facebook'],
+		'ayar_twitter' => $_POST['ayar_twitter'],
+		'ayar_google' => $_POST['ayar_google'],
+		'ayar_youtube' => $_POST['ayar_youtube']
+	));
+
+
+	if ($update) {
+
+		header("Location:../production/sosyal-ayarlar.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/sosyal-ayarlar.php?durum=no");
+	}
+	
+}
+
+if (isset($_POST['mailayarkaydet'])) {
+	
+	$ayarkaydet=$db->prepare("UPDATE ayar SET
+		
+		kullanici_tc=:kullanici_tc,
+		kullanici_adsoyad=:kullanici_adsoyad,
+		kullanici_durum=:kullanici_durum
+		WHERE ayar_id=0");
+
+	$update=$ayarkaydet->execute(array(
+
+		'kullanici_tc' => $_POST['kullanici_tc'],
+		'kullanici_adsoyad' => $_POST['kullanici_adsoyad'],
+		'kullanici_durum' => $_POST['kullanici_durum']
+	));
+
+
+	if ($update) {
+
+		header("Location:../production/mail-ayarlar.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/mail-ayarlar.php?durum=no");
+	}
+	
+}
+
+if (isset($_POST['hakkimizdakaydet'])) {
+	
+	$ayarkaydet=$db->prepare("UPDATE hakkimizda SET
+		hakkimizda_baslik=:hakkimizda_baslik,
+		hakkimizda_icerik=:hakkimizda_icerik,
+		hakkimizda_video=:hakkimizda_video,
+		hakkimizda_vizyon=:hakkimizda_vizyon,
+		hakkimizda_misyon=:hakkimizda_misyon
+		WHERE hakkimizda_id=0");
+
+	$update=$ayarkaydet->execute(array(
+		'hakkimizda_baslik' => $_POST['hakkimizda_baslik'],
+		'hakkimizda_icerik' => $_POST['hakkimizda_icerik'],
+		'hakkimizda_video' => $_POST['hakkimizda_video'],
+		'hakkimizda_vizyon' => $_POST['hakkimizda_vizyon'],
+		'hakkimizda_misyon' => $_POST['hakkimizda_misyon']
+	));
+
+
+	if ($update) {
+
+		header("Location:../production/hakkimizda.php?durum=ok");
+
+	} else {
+
+		header("Location:../production/hakkimizda.php?durum=no");
+	}
+	
+}
+
+if (isset($_POST['kullaniciduzenle'])) {
+
+	$kullanici_id=$_POST['kullanici_id'];
+
+	$ayarkaydet=$db->prepare("UPDATE kullanici SET
+		kullanici_tc=:kullanici_tc,
+		kullanici_adsoyad=:kullanici_adsoyad,
+		kullanici_durum=:kullanici_durum
+		WHERE kullanici_id={$_POST['kullanici_id']}");
+
+	$update=$ayarkaydet->execute(array(
+		'kullanici_tc' => $_POST['kullanici_tc'],
+		'kullanici_adsoyad' => $_POST['kullanici_adsoyad'],
+		'kullanici_durum' => $_POST['kullanici_durum']
+	));
+
+
+	if ($update) {
+
+		Header("Location:../production/kullanici-duzenle.php?kullanici_id=$kullanici_id&durum=ok");
+
+	} else {
+
+		Header("Location:../production/kullanici-duzenle.php?kullanici_id=$kullanici_id&durum=no");
+	}
+
+}
+
+if ($_GET['kullanicisil']=="ok") {
+
+	$sil=$db->prepare("DELETE from kullanici where kullanici_id=:id");
+	$kontrol=$sil->execute(array(
+		'id' => $_GET['kullanici_id']
+	));
+
+
+	if ($kontrol) {
+
+
+		header("location:../production/kullanici.php?sil=ok");
+
+
+	} else {
+
+		header("location:../production/kullanici.php?sil=no");
+
+	}
+
+
+}
+
+?>
